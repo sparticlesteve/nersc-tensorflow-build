61ae59cea5001b5d91185d27df8d3c31063f4ee1
Steve Farrell <sparticlesteve@gmail.com>
2019-08-29 11:22:33 -0700

    fix tensorflow-gpu install

--- build_tensorflow_conda.sh
+++ build_tensorflow_conda.sh
@@ -3,5 +3,5 @@
 . config.sh
 conda activate $INSTALL_DIR
 
-# Install tensorflow with MKL-DNN
-conda install -y -c anaconda tensorflow=${TF_VERSION:1}
+# Install tensorflow with CUDA
+conda install -y -c anaconda tensorflow-gpu=${TF_VERSION:1}
