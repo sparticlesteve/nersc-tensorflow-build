c9830696a3a9da94d1e134bd3e58653644994d07
Steve Farrell <sparticlesteve@gmail.com>
2021-07-29 20:14:03 -0700

    Better solution for conda activate
    Removed the export from previous commit. I'd have to import all of the
    functions from the conda.sh.
    
    Now I set CONDA_INIT_SCRIPT so I can easily source it if needed in any
    downstream build scripts. This should be more maintainable.

--- build_env.sh
+++ build_env.sh
@@ -5,6 +5,7 @@ conda create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION \
     mkl mkl-include cmake cffi typing
 
 # Install additional dependencies via pip
+source $CONDA_INIT_SCRIPT
 conda activate $INSTALL_DIR
 pip install numpy pyyaml setuptools h5py ipython ipykernel matplotlib \
     scikit-learn pandas pillow ipympl
