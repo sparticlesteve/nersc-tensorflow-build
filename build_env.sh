74f51621d1b0b4290ebe1cdc79c4855382d07235
Steve Farrell <sparticlesteve@gmail.com>
2021-08-10 15:34:33 -0700

    add packages from pytorch build scripts

--- build_env.sh
+++ build_env.sh
@@ -7,5 +7,6 @@ conda create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION \
 # Install additional dependencies via pip
 source $CONDA_INIT_SCRIPT
 conda activate $INSTALL_DIR
-pip install --no-cache-dir numpy pyyaml setuptools h5py ipython ipykernel matplotlib \
-    scikit-learn pandas pillow ipympl
+pip install --no-cache-dir numpy pyyaml setuptools h5py ipython ipykernel ray \
+    matplotlib scikit-learn pandas pillow ipympl tdqm wandb gpustat \
+    tensorboard git+https://github.com/NERSC/nersc-tensorboard-helper.git
