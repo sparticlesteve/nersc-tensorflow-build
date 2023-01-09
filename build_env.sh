0dcb2e08c731ad0a336b09b218316fdab0bd1cce
Peter Harrington <pharrington@lbl.gov>
2023-01-09 14:12:21 -0800

    explicit raytune install

--- build_env.sh
+++ build_env.sh
@@ -7,6 +7,6 @@ conda create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION \
 # Install additional dependencies via pip
 source $CONDA_INIT_SCRIPT
 conda activate $INSTALL_DIR
-pip install --no-cache-dir numpy pyyaml setuptools h5py ipython ipykernel ray \
+pip install --no-cache-dir numpy pyyaml setuptools h5py ipython ipykernel ray ray[tune] \
     matplotlib scikit-learn pandas pillow ipympl tdqm wandb gpustat \
     tensorboard git+https://github.com/NERSC/nersc-tensorboard-helper.git
