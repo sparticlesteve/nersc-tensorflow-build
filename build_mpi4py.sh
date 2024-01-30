0081ff5ea76cc2563a81df8466554945d9af94b3
Peter Harrington <pharrington@lbl.gov>
2024-01-29 16:06:23 -0800

    tf215 config, CUDA 12

--- build_mpi4py.sh
+++ build_mpi4py.sh
@@ -1,10 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# Download and extract the software
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
-wget https://bitbucket.org/mpi4py/mpi4py/downloads/mpi4py-3.0.3.tar.gz
-tar zxvf mpi4py-3.0.3.tar.gz
-cd mpi4py-3.0.3
+MPICC="cc -shared" pip install -v --force-reinstall --no-cache-dir --no-binary=mpi4py mpi4py
 
-python setup.py build --mpicc=$MPICC
-python setup.py install
--- config_PM_gpu.sh
+++ config_PM_gpu.sh
@@ -3,12 +3,12 @@
 # Configure the installation
 export INSTALL_NAME="tensorflow"
 export PYTHON_VERSION=3.9
-export TF_VERSION="2.12.0"
+export TF_VERSION="2.15.0"
 export HOROVOD_VERSION="0.28.1"
 
 if [ $USER == "swowner" ]; then
     umask 002 # all-readable
-    INSTALL_BASE=/global/common/software/nersc/pm-2022q4/sw
+    INSTALL_BASE=/global/common/software/nersc/pm-stable/sw
 else
     INSTALL_BASE=$SCRATCH/conda
 fi
@@ -18,10 +18,10 @@ export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/$TF_VERSION
 
 
 # PM configuration
-module load PrgEnv-gnu gcc/11.2.0
-module load cudatoolkit/11.7
-module load cudnn/8.9.1_cuda11
-module load nccl/2.17.1-ofi
+module load PrgEnv-gnu gcc-native/12.3
+module load cudatoolkit/12.2
+module load cudnn/8.9.3_cuda12
+module load nccl/2.18.3-cu12
 module load evp-patch
 export BUILD_DIR=${BUILD_DIR}
 export INSTALL_DIR=${INSTALL_DIR}
@@ -31,8 +31,9 @@ export CC=cc
 export MPICC=cc
 
 # Setup conda
-export CONDA_INIT_SCRIPT=/global/common/software/nersc/pm-2021q4/sw/python/3.9-anaconda-2021.11/etc/profile.d/conda.sh
-source $CONDA_INIT_SCRIPT
+module load conda
+
+# Extra flags for TF XLA compilation
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
 
 # Print some stuff
